7b6ddc1ca890ab4a17af9c832fa3cea4d9f582ca
Brenda Pereira <brendacamara@outlook.com>
2020-12-04 22:39:06 -0300

    config babel

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const App = ()=>{
+    return <a href="/"> Meu app</a>
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import ReactDOM from 'react-dom'
+import React from 'react'
+import App from './App'
 
-ReactDOM.render('Teste', document.getElementById('root'))
+ReactDOM.render(
+    <App />, document.getElementById('root')
+)
