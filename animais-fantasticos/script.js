bbf79547f2738979124eedfb850215196ba62269
Brenda Pereira <brendacamara@outlook.com>
2020-12-23 13:52:25 -0300

    accordion

--- animais-fantasticos/script.js
+++ animais-fantasticos/script.js
@@ -1,19 +1,38 @@
-const imgList = document.querySelectorAll(".js-imglist li");
-const tabContent = document.querySelectorAll(".js-tabcontent section");
+function handleMenu() {
+  const imgList = document.querySelectorAll(".js-imglist li");
+  const tabContent = document.querySelectorAll(".js-tabcontent section");
 
-if (imgList.length && tabContent) {
-  tabContent[0].classList.add("active");
+  if (imgList.length && tabContent) {
+    tabContent[0].classList.add("active");
 
-  function handleActive(index) {
-    tabContent.forEach((section) => {
-      section.classList.remove("active");
+    function handleActive(index) {
+      tabContent.forEach((section) => {
+        section.classList.remove("active");
+      });
+      tabContent[index].classList.add("active");
+    }
+
+    imgList.forEach((item, index) => {
+      item.addEventListener("click", () => {
+        handleActive(index);
+      });
     });
-    tabContent[index].classList.add("active");
+  }
+}
+handleMenu()
+
+function handleAccordion(){
+  const accordionContent = document.querySelectorAll('.js-accordion dt')
+  accordionContent[0].classList.add('active')
+  accordionContent[0].nextElementSibling.classList.add('active')
+
+  function handleToggle(){
+    this.classList.toggle('active')
+    this.nextElementSibling.classList.toggle('active')
   }
 
-  imgList.forEach((item, index) => {
-    item.addEventListener("click", () => {
-      handleActive(index);
-    });
-  });
+  accordionContent.forEach((item)=>{
+    item.addEventListener('click', handleToggle)
+  })
 }
+handleAccordion()
