8bf62822c19195f660db10874e9dfeae474d1087
Brenda Pereira <brendacamara@outlook.com>
2020-12-22 21:15:55 -0300

    site com js,html e css

---
+++ animais-fantasticos/script.js
@@ -0,0 +1,19 @@
+const imgList = document.querySelectorAll(".js-imglist li");
+const tabContent = document.querySelectorAll(".js-tabcontent section");
+
+if (imgList.length && tabContent) {
+  tabContent[0].classList.add("active");
+
+  function handleActive(index) {
+    tabContent.forEach((section) => {
+      section.classList.remove("active");
+    });
+    tabContent[index].classList.add("active");
+  }
+
+  imgList.forEach((item, index) => {
+    item.addEventListener("click", () => {
+      handleActive(index);
+    });
+  });
+}
