704d6c0e0e7282e25033b2187fdcf313df422810
Brenda Pereira <brendacamara@outlook.com>
2020-12-30 13:58:22 -0300

    modal

--- animais-fantasticos/js/script.js
+++ animais-fantasticos/js/script.js
@@ -2,7 +2,9 @@ import smoothScroll from './modules/smoothScroll.js'
 import scrollAnimation from './modules/scrollAnimation.js'
 import handleAccordion from './modules/faqAccordion.js'
 import handleMenu from './modules/menu.js'
+import initModal from './modules/initModal.js'
 
+initModal()
 smoothScroll()
 scrollAnimation()
 handleAccordion()
