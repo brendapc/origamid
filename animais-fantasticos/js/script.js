f2f921c185e7fc2d9e1ad29e7df97abc2ddc7495
Brenda Pereira <brendacamara@outlook.com>
2020-12-30 13:21:23 -0300

    modularizando o código js

---
+++ animais-fantasticos/js/script.js
@@ -0,0 +1,9 @@
+import smoothScroll from './modules/smoothScroll.js'
+import scrollAnimation from './modules/scrollAnimation.js'
+import handleAccordion from './modules/faqAccordion.js'
+import handleMenu from './modules/menu.js'
+
+smoothScroll()
+scrollAnimation()
+handleAccordion()
+handleMenu()
