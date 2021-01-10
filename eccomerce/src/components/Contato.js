1178d734ef49cb5696692becb34fd07850591ecf
Brenda Pereira <brendacamara@outlook.com>
2021-01-09 21:11:59 -0300

    header css

--- eccomerce/src/components/Contato.js
+++ eccomerce/src/components/Contato.js
@@ -1,12 +1,21 @@
-import React from 'react'
-import styles from './Contato.module.css'
+import React from "react";
+import styles from "./Contato.module.css";
+import img from "../img/contato.jpg";
 
 const Contato = () => {
-    return (
-        <div className={styles.header}>
-            <h1>Contato</h1>
-        </div>
-    )
-}
+  return (
+    <div className={styles.contato}>
+      <img src={img} />
+      <div>
+        <h1>Entre em contato;</h1>
+        <ul className={styles.dados}>
+            <li>brenda@outlook.com</li>
+            <li>01010-0101</li>
+            <li>Rua é as guria, 1903</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-export default Contato
+export default Contato;
